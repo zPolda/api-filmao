94dca695484c248bfff8fcc7f89608f550bd7a23
Lucas Cavalli <108158600+zPolda@users.noreply.github.com>
2023-10-04 17:38:03 -0300

    adiciona classes e instancia repositorio

---
+++ FilmesApi/Data/Dtos/CreateEnderecoDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos
+{
+    public class CreateEnderecoDto
+    {
+        public string Logradouro { get; set; }
+        public int Numero { get; set; }
+    }
+}
---
+++ FilmesApi/Data/FilmeContext.cs
@@ -0,0 +1,20 @@
+﻿using FilmesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesAPI.Data;
+
+public class FilmeContext : DbContext
+
+{
+    public FilmeContext(DbContextOptions<FilmeContext> opts) : base(opts)
+    {
+
+    }
+
+    public DbSet<Filme> Filmes { get; set; }
+    public DbSet<Cinema> Cinemas { get; set; }
+    public DbSet<Endereco> Enderecos { get; set; }
+    public DbSet<Sessao> Sessoes { get; set; }
+
+
+}
